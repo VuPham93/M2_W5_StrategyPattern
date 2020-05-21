829af40a700bf3b977db97c9b8b11e5f85b7b7c1
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-05-21 09:19:01 +0700

    Initial commit

---
+++ src/FOO.java
@@ -0,0 +1,25 @@
+@SuppressWarnings("ALL")
+public class FOO extends StrategySolution{
+    private int state = 1;
+    @Override
+    void start() {
+        System.out.print("Start  ");
+    }
+
+    @Override
+    boolean nextTry() {
+        System.out.print("NextTry-" + state++ + "  ");
+        return true;
+    }
+
+    @Override
+    boolean isSolution() {
+        System.out.print("IsSolution-" + (state == 3) + "  ");
+        return (state == 3);
+    }
+
+    @Override
+    void stop() {
+        System.out.println("Stop");
+    }
+}
---
+++ src/StrategySolution.java
@@ -0,0 +1,14 @@
+@SuppressWarnings("ALL")
+public abstract class StrategySolution implements Strategy {
+
+    public void solve() {
+        start();
+        while (nextTry() && !isSolution()) {}
+        stop();
+    }
+
+    abstract void start();
+    abstract boolean nextTry();
+    abstract boolean isSolution();
+    abstract void stop();
+}
